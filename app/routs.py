d121ffd59470af33b1b79a5fa3458ab1915ea511
Vyacheslav Korotach <vkorotach@gmail.com>
2019-01-25 21:14:14 +0200

    EditProfileForm added

--- app/routs.py
+++ app/routs.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user
 from app.models import User
 from flask_login import login_required, logout_user
 from flask import request
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 @app.route('/')
@@ -36,11 +37,11 @@ def login():
     if current_user.is_authenticated:
         return redirect('/index')
     form = LoginForm()
-#    if form.validate_on_submit():
+    #    if form.validate_on_submit():
     if form.is_submitted():
         user = User.query.filter_by(username=form.username.data).first()
-#        flash(user.check_password('cat'))
-#        return redirect('/login')
+        #        flash(user.check_password('cat'))
+        #        return redirect('/login')
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect('/login')
@@ -74,3 +75,37 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect('/login')
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Pridile', form=form)
